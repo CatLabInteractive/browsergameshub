44d9b53e12a7eaed76d4d9e5d4057cce18119a5f
daedeloth <daedeloth@972f61d5-ea06-46ac-8161-7023f7908e50>
2008-09-23 23:30:10 +0000

    git-svn-id: https://catlab.slsapp.com/source/browsergameshub/trunk@68 972f61d5-ea06-46ac-8161-7023f7908e50

--- php/Pages/Validator.php
+++ php/Pages/Validator.php
@@ -65,7 +65,7 @@ class Pages_Validator extends Pages_Page
 					$id = $chk[0]['b_id'];
 				}
 				
-				$page->set ('name', urlencode ($server->getData ('name')));
+				$page->set ('name', htmlentities ($server->getData ('name')));
 				$page->set ('id', $id);
 				
 				$server->updateCache ($id);
