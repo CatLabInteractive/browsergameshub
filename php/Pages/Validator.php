72876f876568f2771977121b72d34e6a97f91efb
daedeloth <daedeloth@972f61d5-ea06-46ac-8161-7023f7908e50>
2008-11-03 19:01:44 +0000

    fix
    git-svn-id: https://catlab.slsapp.com/source/browsergameshub/hub/trunk@221 972f61d5-ea06-46ac-8161-7023f7908e50

--- php/Pages/Validator.php
+++ php/Pages/Validator.php
@@ -4,7 +4,7 @@ class Pages_Validator extends Pages_Page
 	public function getContent ()
 	{
 		// Check for input
-		$url = Core_Tools::getInput ('_REQUEST', 'infourl', 'varchar');
+		$url = Core_Tools::getInput ('_GET', 'infourl', 'varchar');
 		
 		if ($url)
 		{
